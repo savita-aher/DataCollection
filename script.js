663b20a47676ab246d5921e1e86f7eacaa00b77c
savita-aher <savigahire@gmail.com>
2025-08-05 00:09:51 -0400

    few changes

--- script.js
+++ script.js
@@ -5,9 +5,33 @@ let csvStr = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63
 // Split the string into rows
 let rows = csvStr.split("\n");
 
+
 // Loop through each row
 for (let i = 0; i < rows.length; i++) 
     {
     let cells = rows[i].split(",");
     console.log(cells[0], cells[1], cells[2], cells[3]);
     }
+
+
+
+
+//Part 5: Full Circle
+
+const dataObjects = [
+  { id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+  { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+  { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+  { id: "98", name: "Bill", occupation: "Doctor’s Assistant", age: "26" }
+];
+
+
+// Start with headers
+let csvString = "ID,Name,Occupation,Age\\n";
+
+// Add each object's data row
+csvString += dataObjects.map(obj =>
+  `${obj.id},${obj.name},${obj.occupation},${obj.age}`.concat("\\n")
+);
+
+console.log(csvString);
